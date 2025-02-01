9cedb8c7911be580b61ed81473219719b61d4aa8
Mariana Morales <marianamorales2110@gmail.com>
2025-02-01 16:37:06 -0400

    cambios finales

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -13,27 +13,28 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center" 
-    style={{ 
-    backgroundImage: `url(https://images.theconversation.com/files/582820/original/file-20240319-30-8sm5fb.jpg?ixlib=rb-4.1.0&rect=0%2C189%2C3957%2C1978&q=45&auto=format&w=1356&h=668&fit=crop)`, 
-    backgroundSize: 'cover', 
-    backgroundPosition: 'center', 
-    backgroundRepeat: 'no-repeat', 
-    }}>
-    <div style={{ 
-      position: 'absolute', 
-      top: 0, 
-      left: 0, 
-      right: 0, 
-      bottom: 0, 
-      backgroundColor: 'rgba(0, 0, 0, 0.6)',
-      zIndex: 1,
-    }}>
-    </div>
-      <CContainer style={{zIndex:2}}>
+    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center"
+      style={{
+        backgroundImage: `url(https://images.theconversation.com/files/582820/original/file-20240319-30-8sm5fb.jpg?ixlib=rb-4.1.0&rect=0%2C189%2C3957%2C1978&q=45&auto=format&w=1356&h=668&fit=crop)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+        zIndex: 1,
+      }}>
+      </div>
+      <CContainer style={{ zIndex: 2 }}>
         <CRow className="justify-content-center">
           <CCol md={9} lg={7} xl={6}>
             <CCard className="mx-4">
@@ -72,7 +73,10 @@ const Register = () => {
                     />
                   </CInputGroup>
                   <div className="d-grid">
-                    <CButton style={{backgroundColor:'#68ddbd'}}>Crear Usuario</CButton>
+                    <Link to="/dashboard">
+                      <CButton style={{ backgroundColor: '#68ddbd' }}>Crear Usuario</CButton>
+                    </Link>
+
                   </div>
                 </CForm>
               </CCardBody>
