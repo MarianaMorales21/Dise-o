985edab72b670cdfde1a857e22315139c3f6718a
Mariana Morales <marianamorales2110@gmail.com>
2025-02-01 11:22:07 -0400

    cambios

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -16,30 +16,32 @@ import {
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
+
+
 const Login = () => {
   return (
-    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center" 
-    style={{ 
-    backgroundImage: `url(https://images.theconversation.com/files/582820/original/file-20240319-30-8sm5fb.jpg?ixlib=rb-4.1.0&rect=0%2C189%2C3957%2C1978&q=45&auto=format&w=1356&h=668&fit=crop)`, 
-    backgroundSize: 'cover', 
-    backgroundPosition: 'center', 
-    backgroundRepeat: 'no-repeat', 
-    }}>
-    <div style={{ 
-      position: 'absolute', 
-      top: 0, 
-      left: 0, 
-      right: 0, 
-      bottom: 0, 
-      backgroundColor: 'rgba(0, 0, 0, 0.6)',
-      zIndex: 1,
-    }}>
-    </div>
+    <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center"
+      style={{
+        backgroundImage: `url(https://images.theconversation.com/files/582820/original/file-20240319-30-8sm5fb.jpg?ixlib=rb-4.1.0&rect=0%2C189%2C3957%2C1978&q=45&auto=format&w=1356&h=668&fit=crop)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+        zIndex: 1,
+      }}>
+      </div>
       <CContainer style={{ zIndex: 2 }}>
         <CRow className="justify-content-center">
           <CCol md={8}>
             <CCardGroup>
-              <CCard className="p-4" style={{backgroundColor:'#03bb85'}}>
+              <CCard className="p-4" style={{ backgroundColor: '#03bb85' }}>
                 <CCardBody>
                   <CForm>
                     <h1>Iniciar sesión</h1>
@@ -62,9 +64,15 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton className="px-4" style={{backgroundColor:'#9ce0db'}}>
-                          Ingresar
-                        </CButton>
+
+
+
+                        <Link to="/dashboard">
+                          <CButton className="px-4" style={{ backgroundColor: '#9ce0db' }} >
+                            Ingresar
+                          </CButton>
+                        </Link>
+
                       </CCol>
                       <CCol xs={6} className="text-right">
                         <CButton color="black" className="px-0">
@@ -80,10 +88,10 @@ const Login = () => {
                   <div>
                     <h2>Registrarse</h2>
                     <p>
-                    ¿No estas registrado?. Hazlo ahora y tendrás acceso a todos nuestros servicios permitiéndote tener información al instante de los mismo.
+                      ¿No estas registrado?. Hazlo ahora y tendrás acceso a todos nuestros servicios permitiéndote tener información al instante de los mismo.
                     </p>
                     <Link to="/register">
-                      <CButton className="mt-3" tabIndex={-1} style={{backgroundColor:'#9ce0db'}}>
+                      <CButton className="mt-3" tabIndex={-1} style={{ backgroundColor: '#9ce0db' }}>
                         Registrate Ahora!!
                       </CButton>
                     </Link>
